5e24b99d8c66c8ea0060cf9603f286c4876fb1bc
NeverEvi <thhjp.dekimasen@gmail.com>
2023-05-17 20:54:04 -0400

    project 12, adjustment 12

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const { addId } = require("./middlewares/middlewares");
 
 const { PORT = 3001 } = process.env;
 const app = express();
 
 mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
-app.use(addId);
+app.use((req, res, next) => {
+  req.userId = {
+    _id: "6464f05fa4cee3ff70ab6a2b",
+  };
+  next();
+});
 const routes = require("./routes");
 
 app.use(express.json());
--- middlewares/middlewares.js
+++
@@ -1,7 +0,0 @@
-const addId = (req, res, next) => {
-  req.userId = {
-    _id: "6464f05fa4cee3ff70ab6a2b",
-  };
-  next();
-}
-module.exports = {addId}
