0e81c83e9e0fc138689070e715233a8273622822
NeverEvi <thhjp.dekimasen@gmail.com>
2023-10-28 17:56:31 -0400

    move routes

--- app.js
+++ app.js
@@ -10,35 +10,16 @@ require("dotenv").config();
 
 const { PORT = 3000 } = process.env;
 const app = express();
+const routes = require("./routes");
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/wtwr_db")
   .then(() => console.log("Connected to mongoDB"))
   .catch((error) => console.log("Couldn't connect to DB, error: ", error));
 
-const routes = require("./routes");
-
 app.use(express.json());
 app.use(cors());
 app.options("*", cors());
-
-/*app.use((req, res, next) => {
-  const allowedOrigins = [
-    "http://localhost:3000",
-    "https://api.wtwr.strangled.net",
-    "https://wtwr.strangled.net",
-  ];
-  const { origin } = req.headers;
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-  }
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
-  next();
-});*/
 
 app.use(requestLogger);
 
