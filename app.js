bd4793577adb8aafaecf9eec8917edf2dd50aecb
NeverEvi <thhjp.dekimasen@gmail.com>
2023-10-28 14:33:04 -0400

    adjustment 1

--- app.js
+++ app.js
@@ -4,16 +4,19 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const { errors } = require("celebrate");
 const { requestLogger, errorLogger } = require("./middlewares/logger");
-const routes = require("./routes/index");
+const router = require("./routes/index");
 const errorHandler = require("./middlewares/error-handler");
 require("dotenv").config();
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
-mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
+mongoose
+  .connect("mongodb://127.0.0.1:27017/wtwr_db")
+  .then(() => console.log("Connected to mongoDB"))
+  .catch((error) => console.log("Couldn't connect to DB, error: ", error));
 
-//const routes = require("./routes");
+const routes = require("./routes");
 
 app.use(express.json());
 app.use(cors());
@@ -21,6 +24,7 @@ app.options("*", cors());
 
 app.use(requestLogger);
 
+app.use("/", router);
 app.use(routes);
 
 app.use(errorLogger);
