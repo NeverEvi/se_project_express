fc3ae23cb54b69db8344f4a7159f60d78b7576a6
NeverEvi <thhjp.dekimasen@gmail.com>
2023-10-28 16:13:37 -0400

    fixed missing brackets on arrow function

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,11 @@ const clothingItem = require("./clothingItem");
 
 const userInfo = require("./userInfo");
 const { createUser, login } = require("../controllers/userInfo");
-const { validateAuth, validateUserCreation } = require("../middlewares/validation");
-const NotFoundError = require("../errors/NotFoundError")
+const {
+  validateAuth,
+  validateUserCreation,
+} = require("../middlewares/validation");
+const NotFoundError = require("../errors/NotFoundError");
 
 router.post("/signup", validateUserCreation, createUser);
 router.post("/signin", validateAuth, login);
@@ -12,8 +15,8 @@ router.post("/signin", validateAuth, login);
 router.use("/items", clothingItem);
 router.use("/users", userInfo);
 
-router.use(req, res, next) => {
+router.use((req, res, next) => {
   next(new NotFoundError("Router not found"));
-};
+});
 
 module.exports = router;
